97c5d608315c50f0c9ce30d5f077879b66918447
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-21 01:09:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
-    doc = Nokogiri::HTML
+    html = index_url
+    doc = Nokogiri::HTML(open(html))
+
+    
   end
 
   def self.scrape_profile_page(profile_url)
