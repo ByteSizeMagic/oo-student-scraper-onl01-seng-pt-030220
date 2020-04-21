6d78c83299d679e5bfc884ce5306910d516a83c7
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-21 01:15:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,27 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
 
+    student[:profile_quote] = doc.css(".profile-quote").text
+    student[:bio] = doc.css(".description-holder p").text
+
+
+    social_media = doc.css(".social-icon-container a")
+    social_media.each do |social_type|
+      link = social_type.attr('href')
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+    end
+    student
   end
 
 end
